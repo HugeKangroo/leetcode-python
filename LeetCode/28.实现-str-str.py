f72df9cf7e39d3dcfddd0c7bd7a861ec76c41f0a
zy871125746 <zy871125746@gmail.com>
2020-04-01 12:12:08 +0800

    update

--- LeetCode/28.实现-str-str.py
+++ LeetCode/28.实现-str-str.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2020-04-01 10:32:26
-@LastEditTime: 2020-04-01 10:34:58
+@LastEditTime: 2020-04-01 12:09:31
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /Algrithm/LeetCode/28.实现-str-str.py
@@ -15,5 +15,19 @@
 # @lc code=start
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) == 0: return 0
+        for ith in range(len(haystack)):
+            repeatNum = 0
+            for jth in range(len(needle)):
+                if haystack[ith] == needle[jth]:
+                    repeatNum += 1
+                else:
+                    break
+            if repeatNum == len(needle):
+                return ith
+            else:
+                continue
+        return -1
+
 # @lc code=end
 
