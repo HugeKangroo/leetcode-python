8fff67cd26532d621533c4e2f2a8a676f242a7c4
zhaiyu <zhaiyu@efort.com.cn>
2020-04-01 10:35:03 +0800

    update

---
+++ LeetCode/27.移除元素.py
@@ -0,0 +1,42 @@
+'''
+@Author: your name
+@Date: 2020-04-01 09:48:13
+@LastEditTime: 2020-04-01 10:30:04
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: /Algrithm/LeetCode/27.移除元素.py
+'''
+#
+# @lc app=leetcode.cn id=27 lang=python3
+#
+# [27] 移除元素
+#
+
+# @lc code=start
+class Solution:
+    def removeElement(self, nums, val):
+        _idx = 0
+        ith = 0
+        N = len(nums)
+        while ith < N:
+            ith_num = nums[ith]
+            removeNum = 0
+
+            for jth in range(ith+1,N):
+                if nums[jth] == val:
+                    removeNum += 1
+                else:
+                    break
+            
+            ith += 1 if removeNum == 0 else removeNum + 1
+            if ith_num != val: 
+                nums[_idx] = ith_num
+                _idx += 1
+        return _idx
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    so = Solution()
+    so.removeElement([3,2,2,3],3)
---
+++ LeetCode/28.实现-str-str.py
@@ -0,0 +1,19 @@
+'''
+@Author: your name
+@Date: 2020-04-01 10:32:26
+@LastEditTime: 2020-04-01 10:34:58
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: /Algrithm/LeetCode/28.实现-str-str.py
+'''
+#
+# @lc app=leetcode.cn id=28 lang=python3
+#
+# [28] 实现 strStr()
+#
+
+# @lc code=start
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+# @lc code=end
+
