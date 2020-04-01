288dc61ca43bf43ba7ee8a76b5a3ea5cee76c202
zhaiyu <zhaiyu@efort.com.cn>
2020-04-01 18:14:26 +0800

    update

--- LeetCode/35.搜索插入位置.py
+++ LeetCode/35.搜索插入位置.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2020-04-01 15:59:44
-@LastEditTime: 2020-04-01 17:40:28
+@LastEditTime: 2020-04-01 18:14:17
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /Algrithm/LeetCode/35.搜索插入位置.py
@@ -14,19 +14,32 @@
 
 # @lc code=start
 class Solution:
-    def searchInsert(self, nums, target):
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
-            elif nums[i] < target:
-                continue
-            else:
-                return 0 if i == 0 else i
-        return len(nums)
+    # def searchInsert(self, nums, target):
+    #     for i in range(len(nums)):
+    #         if nums[i] == target:
+    #             return i
+    #         elif nums[i] < target:
+    #             continue
+    #         else:
+    #             return i
+    #     return len(nums)
 
-
+    
+    def searchInsert(self,nums,target):
+        head = 0
+        tail = len(nums) - 1
         
+        while True:
+            _idx = round((head + tail) / 2)
+            if nums[_idx] == target:
+                return _idx
+            elif nums[_idx] > target:
+                tail = _idx
+            else:
+                head = _idx
+           
+
 # @lc code=end
 if __name__ == "__main__":
     so = Solution()
-    so.searchInsert([1,3,5,6], 7)
+    so.searchInsert([1,3,5,6], 2)
